42a1c354545926fa1378730ff95f011f66aaf49f
yjt <961549745@qq.com>
2016-11-19 12:26:15 +0800

    精简未实现操作

--- src/doc/Img.java
+++ src/doc/Img.java
@@ -1,15 +1,6 @@
 package doc;
 
 public abstract class Img extends AbsNode {
-	private String base64;
-
-	public String getBase64() {
-		return base64;
-	}
-
-	public void setBase64(String base64) {
-		this.base64 = base64;
-	}
 
 	@Override
 	public void addChild(DocNode node) {
--- src/ui/abs/AbsImgCtrl.java
+++ src/ui/abs/AbsImgCtrl.java
@@ -1,13 +1,9 @@
 package ui.abs;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-
-import com.Ostermiller.util.Base64;
 
 import ui.IImgCtrl;
 import ui.impl.swing.JImg;
@@ -36,28 +32,7 @@ public abstract class AbsImgCtrl extends AbsDocAttrCtrl implements IImgCtrl {
 			ImageIcon icon = new ImageIcon(ImageIO.read(file));
 			JImg img = new JImg();
 			img.setIcon(icon);
-			img.setBase64(encodeImg(file));
 			return img;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	private String encodeImg(File in) {
-		try {
-			File out = new File(System.getProperty("java.io.tmpdir"), ""
-					+ Math.random() + ".base64");
-			Base64.encode(in, out, false);
-
-			BufferedReader reader = new BufferedReader(new FileReader(out));
-			StringBuffer buffer = new StringBuffer();
-			while (reader.ready()) {
-				buffer.append(reader.readLine());
-			}
-			reader.close();
-			out.delete();
-			return buffer.toString();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
